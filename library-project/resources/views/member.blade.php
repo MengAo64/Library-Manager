717333a650ba2edb8819fa5e91b928ab01af9771
zaki240 <mzakiputra24@gmail.com>
2022-10-15 20:53:25 +0700

    member

--- library-project/resources/views/member.blade.php
+++ library-project/resources/views/member.blade.php
@@ -37,15 +37,6 @@
       <td>Mark</td>
       <td>Otto</td>
     </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-    </tr>
   </tbody>
 </table>
 </div>
