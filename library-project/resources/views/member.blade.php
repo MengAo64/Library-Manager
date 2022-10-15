d6a1f14c9647f406d587543fc49d0621415c7b3d
zaki240 <mzakiputra24@gmail.com>
2022-10-15 20:29:48 +0700

    member

---
+++ library-project/resources/views/member.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="member.css">
+    <title>Document</title>
+</head>
+<body>
+<nav>
+        <a href="#"><img src="img/logo.jpeg" alt="logo" width="240px"></a>
+        <ul>
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="#">Manage Books</a></li>
+            <li><a href="#">Manage Members</a></li>
+            <li><a href="#">Records</a></li>
+        </ul>
+    </nav>
+<div class="kotak">
+        <div class="judul"><b>Members</b></div> 
+        <div class="addbook">
+            <a href="#">AddMember</a>
+        </div>
+        <table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Name</th>
+      <th scope="col">Join Date</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+
+</body>
+</html>
