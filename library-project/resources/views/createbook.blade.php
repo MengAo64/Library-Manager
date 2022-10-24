2d95c35c2db30144a966f038af3aa2b27fd7addf
samue <samueluno64@gmail.com>
2022-10-24 10:50:02 +0700

    route update

--- library-project/resources/views/createbook.blade.php
+++ library-project/resources/views/createbook.blade.php
@@ -12,9 +12,9 @@
         <a href="#"><img src="img/logo.jpeg" alt="logo" width="240px"></a>
         <ul>
             <li><a href="/home">Dashboard</a></li>
-            <li><a href="#">Manage Books</a></li>
-            <li><a href="#">Manage Members</a></li>
-            <li><a href="#">Records</a></li>
+            <li><a href="/book">Manage Books</a></li>
+            <li><a href="/member">Manage Members</a></li>
+            <li><a href="/record">Records</a></li>
         </ul>
     </nav>
 
