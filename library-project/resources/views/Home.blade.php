64047d64fa23e2c246cbb42fc7ecd016ffac36ce
rizkyramadhan00 <zalzalfaridzal@gmail.com>
2022-11-16 09:18:22 +0700

    update foto

--- library-project/resources/views/Home.blade.php
+++ library-project/resources/views/Home.blade.php
@@ -59,7 +59,7 @@ class="swiper mySwiper2"
                     <span class="overlay"></span>
 
                     <div class="card-image">
-                      <img src="{{ asset("img/m.png") }}" alt="" class="card-img">
+                      <img src="{{ asset("img/farel.jpg") }}" alt="" class="card-img">
                     </div>
                 </div>
 
@@ -104,7 +104,7 @@ class="swiper mySwiper2"
                     <span class="overlay"></span>
 
                     <div class="card-image">
-                        <img src="{{ asset("img/rizky,jpg") }}" alt="" class="card-img">
+                        <img src="{{ asset("img/m.png") }}" alt="" class="card-img">
                     </div>
                 </div>
 
