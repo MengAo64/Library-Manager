3b7ee5e62e55e1e8c74eb91fb46168bf9606c14d
zaki240 <mzakiputra24@gmail.com>
2022-11-17 20:24:41 +0700

    fix-add

--- library-project/resources/views/records/create.blade.php
+++ library-project/resources/views/records/create.blade.php
@@ -14,50 +14,61 @@ Library | AddMember
             <h1>Add Records</h1>
             </div>
 
-        <div class="input">
 
-            <div class="record-borrower"><span class="input-group-text nama-inp">Borrower =</span>
-                <!-- <input class="inputborrower" name="name" type="text" aria-label="First name" class="form-control" placeholder="input Borrower" value="{{old ('name')}}"> -->
+            <div class="container">
+        <div class="input">
+            <div class="borrow">
+            <div class="record-borrower">
+                <label class="input-group-text nama-inp">Borrower</label>
                 <select class="input-borrower" name="member_id" id="">
                     @foreach($members as $member)
                         <option value="{{$member->id}}">{{$member->name}}</option>
                     @endforeach
                 </select>  
-            </div>           
+            </div>        
+        </div>   
 
+
+        <div class="book">
             <div class="book-add">
-                <span class="record-book" id="inputGroup-sizing-lg">Book =</span>
+                <label class="record-book" id="inputGroup-sizing-lg">Book </label>
                 <select class="input-book" name="book_id" id="">
                     @foreach($books as $book)
                         <option value="{{$book->id}}">{{$book->title}}</option>
                     @endforeach
                 </select>
                 </div>
+            </div>
 
 
+
+
+            <div class="date">
             <div class="date-record">
-                <span class="record-date" id="inputGroup-sizing-lg">Date =</span>
+                <label class="record-date" id="inputGroup-sizing-lg">Date</label>
                 <input class="input-date" name="join_date" type="date" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" placeholder="Join Date" value="{{old ('join_date')}}" >
             </div>
                     @error('join_date')
                         <p style="color: red; font-size:13px; margin-left:7px;">{{$message}}</p>
                     @enderror
+        </div>
+
+
 
 
                     <div class="status1">
-                    <div class="status">            
-                <div class="judultitle">Status</div>
+                    <daiv clss="status">            
+                <div class="judultitle"><label> Status</label></div>
                 <div class="stat">
                <div class="input1"><input type="radio"  name="status" value="Dipinjam" >Dipinjam </div> 
                <div class="input2"><input type="radio"  name="status" value="Tidak Dipinjam" {{"checked"}} >Tidak Dipinjam</div> 
             </div>   
         </div>
+    </div>   
+        </div>   
     </div>
-                
-                <button class="btn-submit" type="submit"> Add</button>
-        
-        </div>      
+        <button class="btn-submit" type="submit"> Add</button>   
     </div>            
-       
+</form>
 
 @endsection
