ef3e5b66954e72ad51accd78b704fe63c0bb76f3
zaki240 <mzakiputra24@gmail.com>
2022-11-23 09:23:21 +0700

    edit-member

--- library-project/resources/views/members/edit.blade.php
+++ library-project/resources/views/members/edit.blade.php
@@ -17,27 +17,25 @@ Library | Edit Member
             </div>
             <br> <br>
 
-            <div class="kotak-input">
-
-
-                <div class="nama-add"><span class="input-group-text nama-inp">Name =</span>
-                <input value="{{$member->name}}" class="inputnama" name="name" type="text" aria-label="First name" class="form-control" placeholder="Name">
+            <div class="edit">
+            <div class="kotak-input-1">
+                <div class="nama-add"><span class="input-group-text nama-inp" style="margin-left: 45px">Name  </span>
+                <input value="{{$member->name}}" class="input-nama" name="name" type="text" aria-label="First name" class="form-control" placeholder="   Name">
                 @error('name')
                   <p style="color: red; font-size:13px; margin-left:7px;">{{$message}}</p>
                 @enderror
                 </div>
-                <br><br>
 
-                <div class="date-add">
-                <span class="input-group-text" id="inputGroup-sizing-lg">Join Date =</span>
-                <input value="{{$member->join_date}}" class="inputdate" name="join_date"type="date" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" placeholder="Join Date">
+                <div class="nama-add">
+                <span class="input-group-text" id="inputGroup-sizing-lg">Join Date  </span>
+                <input value="{{$member->join_date}}" class="input-nama" name="join_date"type="date" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" placeholder="Join Date">
                 @error('join_date')
                      <p style="color: red; font-size:13px; margin-left:7px;">{{$message}}</p>
                 @enderror
                 </div>
 
             </div>    
-        <br><br>
+        </div>
         <button class="btn-submit" type="submit"> Update </button>
         
         </div>
